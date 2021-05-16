b1305e0aaf9638eafc481bc0737cb02a66b05b9f
TomClancy-py <71928310+TomClancy-py@users.noreply.github.com>
2021-05-16 13:59:13 +0530

    First commit

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
-alert("Nothing to see here");
-Console.log("Nothing to see here either");
+const correct = document.querySelector(".correct");
+const wrong = document.querySelector(".wrong");
+
+const container = document.querySelector(".container");
+const ques1 = document.getElementById("ques1");
+const ques2 = document.getElementById("ques2");
+const ques3 = document.getElementById("ques3");
+const ques4 = document.getElementById("ques4");
+const ques5 = document.getElementById("ques5");
+
+const main_page = document.getElementById("main");
+
+
+
+container.style.display ="none";
+
+correct.onclick= () => {
+correct.classList.add("correct");
+points += 20;
+}
+
+wrong.onclick = () => {
+wrong.classList.add("wrong");
+points -= 10;
+}
