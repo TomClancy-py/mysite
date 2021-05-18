368c7952a5270c26cddf7b0a76d4c7f7bc43c63d
TomClancy-py <tomclancy5544@gmail.com>
2021-05-18 13:22:32 +0530

    first commit

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
+
 const correct = document.querySelector(".correct");
 const wrong = document.querySelector(".wrong");
 
 const container = document.querySelector(".container");
-const ques1 = document.getElementById("ques1");
-const ques2 = document.getElementById("ques2");
-const ques3 = document.getElementById("ques3");
-const ques4 = document.getElementById("ques4");
-const ques5 = document.getElementById("ques5");
+//const ques1 = document.querySelector(".ques1").style.display = "none";
+const ques2 = document.getElementsByClassName("ques2");
+const ques3 = document.getElementsByClassName("ques3");
+const ques4 = document.getElementsByClassName("ques4");
+const ques5 = document.getElementsByClassName("ques5");
 
+
+//const btn_ques1 = document.getElementsByClassName("btnques1");
+const btn_ques2 = document.getElementsByClassName("btnques2");
+const btn_ques3 = document.getElementsByClassName("btnques3");
+const btn_ques4 = document.getElementsByClassName("btnques4");
+const btn_ques5 = document.getElementsByClassName("btnques5");
+const page = document.getElementsByClassName("page")
 const main_page = document.getElementById("main");
+const play = document.getElementById("Play");
+const game = document.getElementsByClassName("game");
 
 
 
-container.style.display ="none";
-
-correct.onclick= () => {
-correct.classList.add("correct");
-points += 20;
+correct.onclick = function(){
+  points += 20;
+  console.log(points);
 }
 
-wrong.onclick = () => {
-wrong.classList.add("wrong");
-points -= 10;
+wrong.onclick = function(){
+  points -= 10;
+  console.log(points)
+}
+
+function start(){
+  console.log("hello world");
+  document.getElementById("page").style.display = "none";
+  document.getElementById("game").style.display = "block";
+
+document.querySelector(".btn_ques1").style.display = "block";
+
+
 }
